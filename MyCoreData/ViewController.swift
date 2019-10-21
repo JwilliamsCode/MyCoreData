c8e6b3a8f1941cb39886640d88ca4a1724a8e64b
Justin W <s0181433@student.rockvalleycollege.edu>
2019-10-21 17:12:16 -0500

    Initial Commit

---
+++ MyCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCoreData
+//
+//  Created by Justin Williams on 10/21/19.
+//  Copyright © 2019 Justin Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
